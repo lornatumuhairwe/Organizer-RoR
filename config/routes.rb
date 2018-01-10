02f8384cdeb95eac3d755a12d2395af2e327a867
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-01-10 08:00:03 +0300

    initial static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :categories
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
