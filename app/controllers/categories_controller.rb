f0a1aa4f1d1263153f928402e00e6da0ecda8d0b
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-01-11 10:59:37 +0300

    add category functionality and tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,26 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @category = Category.new
+  end
+
+  def new
+  end
+
+  def create
+    @category = Category.new(new_category_params)
+    if @category.save
+      flash[:success] = 'Category created successfully'
+      redirect_to root_url
+    else
+      flash[:success] = 'Category creation failed'
+      redirect_to root_url
+    end
+  end
+
+  private
+
+  def new_category_params
+    params.require(:category).permit(:title, :description)
   end
 end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
 
+  before(:each) do
+    @category = create(:category)
+  end
+
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, params: { id: @category.id }
       expect(response).to have_http_status(:success)
     end
   end
@@ -13,6 +17,20 @@ RSpec.describe CategoriesController, type: :controller do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "add category methods", :type => :request do
+    it 'adds new categories with title' do
+    expect {
+      post categories_path, params: { category: { title: 'T', description: 'blabla' } }
+    }.to change(Category, :count)
+    end
+
+    it 'doesnt add category with title' do
+      expect {
+        post categories_path, params: { category: { title: '', description: 'blabla' } }
+      }.not_to change(Category, :count)
     end
   end
 
