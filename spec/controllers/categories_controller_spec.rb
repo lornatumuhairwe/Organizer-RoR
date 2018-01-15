76aa33c862b1d7fed0fd6653471c17b1b652f8f8
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-01-15 16:44:18 +0300

    devise tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,77 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-  describe "anonymous user" do
-    before(:each) do
-      @category = create(:category)
-      # new_user_session_path(@user)
-      # sign_in nil
-    end
-
-    it "redirects to root" do
-      get :show, params: { id: @category.id }
-      expect( response ).to redirect_to( new_user_session_path )
-    end
-  end
+  # describe "anonymous user" do
+  #   before(:each) do
+  #     @category = create(:category)
+  #     @user = create(:user)
+  #     # new_user_session_path(@user)
+  #     # sign_in nil
+  #   end
+  #
+  #   it "redirects to root" do
+  #     get :show, params: { id: @category.id }
+  #     expect( response ).to redirect_to( new_user_session_path )
+  #   end
+  # end
 
   describe "signed in user" do
+    let ( :user ) { User.find(1) }
+
     before(:each) do
       @request.env['devise.mapping'] = Devise.mappings[:user]
-      @user = create(:user)
       @category = create(:category)
-      sign_in @user
+      sign_in user
     end
 
-    describe "GET #show" do
-      it "returns http success" do
-        get :show, params: { id: @category.id }
-        expect(response).to have_http_status(:success)
-      end
-    end
+    # describe "GET #show" do
+    #   it "returns http success" do
+    #     get :show, params: { id: @category.id }
+    #     expect(response).to have_http_status(:success)
+    #   end
+    # end
 
-    describe "GET #index" do
-      it "returns http success" do
-        get :index
-        expect(response).to have_http_status(:success)
-      end
-    end
-
+    # describe "GET #index" do
+    #   it "returns http success" do
+    #     get :index
+    #     expect(response).to have_http_status(:success)
+    #   end
+    # end
+  #
     describe "add category methods", type: :request do
+      let (:current_user) { User.find(1) }
+
+      before (:each) do
+        sign_in current_user
+        # byebug
+      end
+
       it 'adds new categories with title' do
+        # post categories_path, params: { category: { title: 'T', description: 'blabla' } }
         expect do
           post categories_path, params: { category: { title: 'T', description: 'blabla' } }
-        end.to change(Category, :count)
+        end.to change { Category.count }.by(1)
+        # expect(flash[:success]).to match(/Category created successfully/)
+
       end
 
-      it 'doesnt add category with title' do
+      it 'doesnt add category without title' do
         expect do
           post categories_path, params: { category: { title: '', description: 'blabla' } }
         end.not_to change(Category, :count)
       end
     end
-
-    # describe "deletes category", type: :request do
-    #   it 'deletes one category' do
-    #     category_id = @category.id
-    #     expect do
-    #       delete "/categories/#{category_id}"
-    #     end.to change(Category, :count)
-    #   end
-    # end
-
-    # describe 'updates category', type: :request do
-    #   before { put "/categories/#{@category.id}", params: { category: { title: 'tester', description: 'the description' } }}
-    #   it 'updates one category' do
-    #     expect(@category.reload.title).to eq('tester')
-    #   end
-    # end
-    #
-    # describe 'cannot update category without title', type: :request do
-    #   before { put "/categories/#{@category.id}", params: { category: { title: '', description: 'the description' } }}
-    #   it 'updates one category' do
-    #     expect(@category.reload.title).to eq('Factory category')
-    #   end
-    # end
+  #
+  #   # describe "deletes category", type: :request do
+  #   #   it 'deletes one category' do
+  #   #     category_id = @category.id
+  #   #     expect do
+  #   #       delete "/categories/#{category_id}"
+  #   #     end.to change(Category, :count)
+  #   #   end
+  #   # end
+  #
+  #   # describe 'updates category', type: :request do
+  #   #   before { put "/categories/#{@category.id}", params: { category: { title: 'tester', description: 'the description' } }}
+  #   #   it 'updates one category' do
+  #   #     expect(@category.reload.title).to eq('tester')
+  #   #   end
+  #   # end
+  #   #
+  #   # describe 'cannot update category without title', type: :request do
+  #   #   before { put "/categories/#{@category.id}", params: { category: { title: '', description: 'the description' } }}
+  #   #   it 'updates one category' do
+  #   #     expect(@category.reload.title).to eq('Factory category')
+  #   #   end
+  #   # end
   end
 
 end
